70f329744e0b14f68f541afea2071f0093eec4db
Cristian Sandu <cristian.sandu@gmail.com>
2017-09-05 20:31:33 +0300

    version 0.0.1

--- xu4-fancontrol.sh
+++ xu4-fancontrol.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+REFRESH="1"
+DISK_TEMP_THRESHOLD="45"
+CPU_TEMP_THRESHOLD="60"
+FAN_CHANGED="*"
+
 get_disk_dev_info() {
-	fdisk -l > disks.txt 2>/dev/null
-	SATA=($(awk '/^\/dev\/sd/ {printf "%s ", $1}' disks.txt))
-	rm disks.txt
+    # Pull disk info from /dev/sd*
+    fdisk -l > disks.txt 2>/dev/null
+    SATA=($(awk '/^\/dev\/sd/ {printf "%s ", $1}' disks.txt))
+    rm disks.txt
 }
 
 get_disk_temperature() {
-	for i in "${!SATA[@]}"
-	do
-		# declare and assign variable seperately to avoid masking return value
-		DISK_TEMP[$i]=" (IDLE)"
+    for i in "${!SATA[@]}"
+    do
+        # declare and assign variable seperately to avoid masking return value
+        DISK_TEMP[$i]=" (IDLE)"
         local t
         t=$(smartctl -a "${SATA[$i]}" -d sat | grep "Temp")
         if (( $? == 0 ))
@@ -19,21 +25,70 @@ get_disk_temperature() {
         else
             DISK_TEMP[$i]=""
         fi
-	done
+    done
 }
 
 get_cpu_temperature() {
-    for i in {0..4} 
+    for i in {0..4}
     do
         _t=$(($(</sys/class/thermal/thermal_zone${i}/temp) / 1000))
         CPU_TEMP[$i]="$_t"
     done
 }
 
-# Pull disk info from /dev/sd*
-get_disk_dev_info
-get_disk_temperature
-get_cpu_temperature
+fan_on() {
+    i2cset -y 1 0x60 0x05 0x00
+}
 
-echo ${DISK_TEMP[@]}
-echo ${CPU_TEMP[@]}
+fan_off() {
+    i2cset -y 1 0x60 0x05 0x05
+}
+
+handle_fan() {
+    for i in "${!DISK_TEMP[@]}"
+    do
+        if (( "${DISK_TEMP[$i]}" > "${DISK_TEMP_THRESHOLD}" ))
+        then
+            if [ "${FAN_CHANGED}" != "1" ]
+            then
+                echo "Turning fan on because disk $i has hit the threshold"
+            fi
+
+            FAN_CHANGED="1"
+            fan_on
+            return
+        fi
+    done
+    
+    for i in "${!CPU_TEMP[@]}"
+    do
+        if (( "${CPU_TEMP[$i]}" > "${CPU_TEMP_THRESHOLD}" ))
+        then
+            if [ "${FAN_CHANGED}" != "1" ]
+            then
+                echo "Turning fan on because CPU $i has hit the threshold"                
+            fi
+
+            FAN_CHANGED="1"
+            fan_on
+            return
+        fi
+    done
+    
+    # No fuss, fan is off
+    if [ "${FAN_CHANGED}" != "0" ]
+    then
+        echo "All temps nominal, turning fan off"
+        FAN_CHANGED="0"
+    fi
+    fan_off
+}
+
+while true; do
+    get_disk_dev_info
+    get_disk_temperature
+    get_cpu_temperature
+    handle_fan
+    
+    sleep ${REFRESH}
+done
